79f7d9b4d54a0f19111885ba89c833addca0b01a
XuZheng <49426295+zhengxuJosh@users.noreply.github.com>
2024-07-31 14:46:50 +0800

    Update demo.py

--- SAM2/demo.py
+++ SAM2/demo.py
@@ -6,7 +6,6 @@ from sam2.sam4ss import sam4ss
 checkpoint = "./checkpoints/sam2_hiera_base_plus.pt"
 model_cfg = "./sam2_hiera_b+.yaml"
 sam2 = build_sam2(model_cfg, checkpoint)
-print(0)
 net = sam4ss(sam2)
 
 input = torch.randn(1,3,1024,1024)
