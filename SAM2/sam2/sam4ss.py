1565e7d16554e542a4eed074ac8b86ab7a65134b
XuZheng <49426295+zhengxuJosh@users.noreply.github.com>
2024-07-31 14:18:07 +0800

    Add files via upload

---
+++ SAM2/sam2/sam4ss.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+from .modeling.sam2_base import SAM2Base
+
+class sam4ss(nn.Module):
+
+    def __init__(self, sam_model: SAM2Base):
+        super(sam4ss, self).__init__()
+
+        self.sam = sam_model
+
+    def forward(self, batched_input, multimask_output=True):
+            image_embedding = self.sam.forward_image(batched_input)
+
+            return self.sam._forward_sam_heads(image_embedding['vision_features'], high_res_features=image_embedding['backbone_fpn'][:2], multimask_output=multimask_output) # , image_size
